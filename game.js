ebafaace3c17be572e4149304e286b8876bb683a
vishnu-dev <thevishnudev@gmail.com>
2017-03-17 11:48:13 +0530

    Ball bouncing off the walls

--- game.js
+++ game.js
@@ -5,11 +5,12 @@ var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10;
 
 function drawBall() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#FF0000";
     ctx.fill();
     ctx.closePath();
 }
@@ -17,6 +18,14 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
     x += dx;
     y += dy;
 }
