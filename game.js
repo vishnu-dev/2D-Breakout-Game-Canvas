ecae4934dfcd00d0074090df195165c969d79303
vishnu-dev <thevishnudev@gmail.com>
2017-05-27 10:09:43 +0530

    Game v1.0

--- game.js
+++ game.js
@@ -29,9 +29,11 @@ for(c=0; c<brickColumnCount; c++) {
     }
 }
 
-// Key-press event listners
+// Event listners
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
 
 function keyDownHandler(e) {
     if(e.keyCode == 39) {
@@ -51,6 +53,13 @@ function keyUpHandler(e) {
     }
     else if(e.keyCode == 37) {
         leftPressed = false;
+    }
+}
+
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
     }
 }
 
